31976676063190de6759aa58082c9670c04e50e9
Junico Tandiago <junicotandiagoo21@gmail.com>
2022-03-18 22:21:52 +0700

    Configurasi MyAuth

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -9,12 +9,12 @@ class Email extends BaseConfig
     /**
      * @var string
      */
-    public $fromEmail;
+    public $fromEmail = 'junicotandiago21@gmail.com';
 
     /**
      * @var string
      */
-    public $fromName;
+    public $fromName = 'junico tandiago';
 
     /**
      * @var string
